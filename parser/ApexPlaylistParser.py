b71c81100de6fb1fa791356640ccb0574ecc5ee6
Zenahr <zenmatica@gmail.com>
2021-09-17 00:50:03 +0200

    parser: add comments

--- parser/ApexPlaylistParser.py
+++ parser/ApexPlaylistParser.py
@@ -3,19 +3,23 @@ import vdf
 # https://github.com/ValvePython/vdf
 # https://developer.valvesoftware.com/wiki/KeyValues
 
+PARSER_FIX_TOKEN = '___SOME_RANDOM_TOKEN___'
+
 def preprocess():
+        """Since Apex does not use VDF strictly we need to replace any '#' we find with a random token so that the standard VDF parser doesn't break."""
     with open(r'test.txt', 'r') as infile, \
          open(r'test-intermediate.txt', 'w') as outfile:
         data = infile.read()
-        data = data.replace("#", "___SOME_RANDOM_TOKEN___")
+        data = data.replace("#", PARSER_FIX_TOKEN)
         outfile.write(data)
         return data
 
 def postprocess():
+        """Revert changes made by preprocess() to revert parser hotfix."""
     with open(r'test-intermediate.txt', 'r') as infile, \
          open(r'test.txt', 'w') as outfile:
         data = infile.read()
-        data = data.replace("___SOME_RANDOM_TOKEN___", "#")
+        data = data.replace(PARSER_FIX_TOKEN, "#")
         outfile.write(data)
         outfile.close()
     os.remove('test-intermediate.txt')
