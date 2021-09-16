7a25a1e0bcc4ce1e151aa989ab6c9f4ff546fa8a
Zenahr <zenmatica@gmail.com>
2021-09-17 01:13:43 +0200

    parser: correctly parse and get playlists

--- parser/ApexPlaylistParser.py
+++ parser/ApexPlaylistParser.py
@@ -4,35 +4,51 @@ import vdf
 # https://developer.valvesoftware.com/wiki/KeyValues
 
 PARSER_FIX_TOKEN = '___SOME_RANDOM_TOKEN___'
+INTERMEDIATE_FILE = 'intermediate.txt'
 
-def preprocess():
-        """Since Apex does not use VDF strictly we need to replace any '#' we find with a random token so that the standard VDF parser doesn't break."""
-    with open(r'test.txt', 'r') as infile, \
-         open(r'test-intermediate.txt', 'w') as outfile:
-        data = infile.read()
-        data = data.replace("#", PARSER_FIX_TOKEN)
-        outfile.write(data)
-        return data
+class ApexPlaylistParser:
+        def __init__(self, filePath):
+                self.__inputFile     = open(filePath, 'r')
+                self.__inputFilePath = filePath
 
-def postprocess():
-        """Revert changes made by preprocess() to revert parser hotfix."""
-    with open(r'test-intermediate.txt', 'r') as infile, \
-         open(r'test.txt', 'w') as outfile:
-        data = infile.read()
-        data = data.replace(PARSER_FIX_TOKEN, "#")
-        outfile.write(data)
-        outfile.close()
-    os.remove('test-intermediate.txt')
-    return data
+        def preprocess(self):
+                """Since Apex does not use VDF strictly we need to replace any '#' we find with a random token so that the standard VDF parser doesn't break."""
+                infile  = self.__inputFile
+                outfile = open(INTERMEDIATE_FILE, 'w')
+                data    = infile.read()
+                data    = data.replace("#", PARSER_FIX_TOKEN)
+                outfile.write(data)
+                outfile.close()
+                return data
 
-def parse():
-        d = vdf.parse(open('test-intermediate.txt'))
-        return d
+        def parse(self):
+                d = vdf.parse(open(INTERMEDIATE_FILE))
+                return d
 
-def write(newData):
-        vdf.dump(newData, open('test.txt','w'), pretty=True)
+        def getPlaylists(self):
+                try:
+                        return vdf.parse(open(INTERMEDIATE_FILE))['playlists']['Playlists']
+                except KeyError:
+                        print('something went wrong. Playlists could not be found.')
+
+        def save(self, newData):
+                vdf.dump(newData, open(self.__inputFilePath,'w'), pretty=True)
+
+        def postprocess(self):
+                """Revert changes made by preprocess() to revert parser hotfix."""
+                with open(INTERMEDIATE_FILE, 'r') as infile, \
+                     open(self.__inputFilePath, 'w') as outfile:
+                    data = infile.read()
+                    data = data.replace(PARSER_FIX_TOKEN, "#")
+                    outfile.write(data)
+                    outfile.close()
+                os.remove(INTERMEDIATE_FILE)
+                return data
+
 
 if __name__ == '__main__':
-    preprocess()
-    parse()
-    postprocess()
+        p = ApexPlaylistParser('./test.txt')
+        p.preprocess()
+        playlists = p.getPlaylists()
+        print(playlists)
+        # p.postprocess()
